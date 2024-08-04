247e2b8e47810ed3551cdc87e5daeb2b1547d4af
nazid <nazidk773@gmail.com>
2024-08-04 13:09:36 +0530

    remove show class

--- app.js
+++ app.js
@@ -3,9 +3,17 @@
 const questions = document.querySelectorAll(".question");
 
 questions.forEach(function(question){
+    // console.log(question);
     const btn = question.querySelector(".question-btn");
-    
-    btn.addEventListner("click",function(){
+    // console.log(btn);
+    btn.addEventListener("click",function(){
+
+        questions.forEach(function(item){
+            if(item !== question)(
+                item.classList.remove("show-text")
+            )
+        });
+
         question.classList.toggle("show-text");
     });
 });

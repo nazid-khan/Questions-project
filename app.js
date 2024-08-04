1f3d6d055cda91bb508748bad48c1c4f2b483271
nazid <nazidk773@gmail.com>
2024-08-04 12:35:53 +0530

    toggle show-text

--- app.js
+++ app.js
@@ -2,3 +2,10 @@
 // traversing the dom
 
 const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach(function(btn){
+    btn.addEventListener("click",function(e){
+        const question = e.currentTarget.parentElement.parentElement;
+        question.classList.toggle("show-text");
+    })
+});
